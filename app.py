9217ecdc5d0922bc5d3b7b8a3a9a492bb4cbd286
mmiyamoto-sg <150712392+mmiyamoto-sg@users.noreply.github.com>
2023-11-13 11:34:04 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,117 @@
+from flask import Flask, render_template, request, redirect, url_for, session, send_from_directory
+from flask_session import Session
+from compare_manifest import (
+    extract_three_digit_number,
+    normalize_section_name,
+    apply_rules,
+    load_csv,
+    compare_manifests
+)
+import csv
+import os
+import pandas as pd
+from io import StringIO
+
+app = Flask(__name__)
+app.config["SESSION_TYPE"] = "filesystem"
+app.config["SECRET_KEY"] = os.urandom(24)  # This is for session encryption
+Session(app)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    differences = []
+
+    if "rules" not in session:
+        session["rules"] = ""
+    
+    if request.method == "POST":
+        """csv_contents1 = csv_contents2 = ''
+        if 'sgmanifest' in request.files:
+            csv_file1 = request.files['sgmanifest']
+
+            if csv_file1.filename != '':
+                csv_contents1 = csv_file1.read().decode('utf-8')
+        
+        if 'clientmanifest' in request.files:
+            csv_file2 = request.files['clientmanifest']
+
+            if csv_file2.filename != '':
+                csv_contents2 = csv_file2.read().decode('utf-8')"""
+        ##uploaded_files = request.files.getlist('manifests')
+        
+        # Make sure we have both manifests
+        """if len(uploaded_files) == 2:
+            for file in uploaded_files:
+                if file.filename == 'seatgeek_manifest.csv':
+                    session['seatgeek_manifest_text'] = file.read().decode('utf-8')
+                elif file.filename == 'client_manifest.csv':
+                    session['client_manifest_text'] = file.read().decode('utf-8')"""
+        if 'csv1' in request.form and 'csv2' in request.form:
+            session['client_manifest_text'] = request.form['csv2']
+            session['seatgeek_manfiest_text'] = request.form['csv1']
+
+            
+            rules_text = request.form['rules']
+            session["rules"] = rules_text
+            session["differences"] = perform_comparison(session.get
+            ('seatgeek_manifest_text', ''), session.get('client_manifest_text', ''), rules_text)
+
+            #print(session['seatgeek_manifest_text'])
+            
+        # Try if statement where upload is 0. Must store csv from first action in variable or text area to be used over and over again within session because POST can't pre-populate input fields due to security
+        
+
+
+    return render_template("index.html", differences=session.get("differences", []), rules=session["rules"])
+
+@app.route('/download/differences', methods=['GET'])
+def download_differences():
+    with open("differences.txt", "w") as f:
+        for diff in session.get("differences", []):
+            f.write(diff + "\n")
+    return send_from_directory('.', 'differences.txt', as_attachment=True, download_name='differences.txt')
+
+@app.route('/download/rules')
+def download_rules():
+    with open("rules.txt", "w") as f:
+        f.write(session.get("rules", ""))
+    return send_from_directory('.', 'rules.txt', as_attachment=True, download_name='rules.txt')
+
+def perform_comparison(seatgeek_text, client_text, rules_text):
+    # Save files temporarily for processing
+    with open("temp_seatgeek.csv", "w") as f:
+        f.write(seatgeek_text)
+    
+    with open("temp_client.csv", "w") as f:
+        f.write(client_text)
+    
+    rules = load_rules_from_text(rules_text)
+
+    seatgeek_manifest_sections = set()
+
+    with open('temp_seatgeek.csv', 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            section_value = row.get('SECTION', row.get('section', '')).lower()
+            seatgeek_manifest_sections.add(section_value)
+        
+    seatgeek_manifest = load_csv('temp_seatgeek.csv', rules, seatgeek_manifest_sections)
+    client_manifest = load_csv('temp_client.csv', rules, seatgeek_manifest_sections)
+
+    return compare_manifests(seatgeek_manifest, client_manifest)
+
+def load_rules_from_text(text):
+    # Adapt the load_rules function to work with direct text input
+    rules = {}
+    lines = text.strip().split("\n")
+    for line in lines:
+        # Check if the line contains the delimiter before splitting
+        if ' > ' in line:
+            pattern, replace = line.strip().split(' > ')
+            pattern = pattern.replace(" ", "-")
+            replace = replace.replace(" ", "-")
+            rules[pattern] = replace
+    return rules
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ compare_manifest.py
@@ -0,0 +1,242 @@
+import csv
+import re
+import logging
+
+# Set up logging
+logging.basicConfig(filename='compare_manifest.log', level=logging.INFO, format='%(asctime)s - %(message)s')
+
+def load_rules(filename):
+    rules = {}
+    with open(filename, 'r') as file:
+        for line in file:
+            pattern, replace = line.strip().split(' > ')
+            logging.info(f"Loaded rule: {pattern} > {replace}")
+            
+            # Replace spaces between alphanumeric boundaries with dashes
+            pattern = re.sub(r'([a-z]) (\d)', r'\1-\2', pattern)
+            pattern = re.sub(r'(\d) ([a-z])', r'\1-\2', pattern)
+            replace = re.sub(r'([a-z]) (\d)', r'\1-\2', replace)
+            replace = re.sub(r'(\d) ([a-z])', r'\1-\2', replace)
+            
+            # Insert dashes between alphanumeric boundaries (without spaces)
+            pattern = re.sub(r'([a-z])(\d)', r'\1-\2', pattern)
+            pattern = re.sub(r'(\d)([a-z])', r'\1-\2', pattern)
+            replace = re.sub(r'([a-z])(\d)', r'\1-\2', replace)
+            replace = re.sub(r'(\d)([a-z])', r'\1-\2', replace)
+            
+            # Replace spaces with dashes in the replacement pattern
+            replace = replace.replace(' ', '-')
+            
+            rules[pattern] = replace
+    return rules
+
+def extract_three_digit_number(section):
+    matches = re.findall(r'\b\d{3}\b', section)
+    return matches[0] if matches else None    
+
+def normalize_section_name(section):
+    """
+    Apply basic normalization to the section name.
+    """
+    original_section = section
+    section = section.lower()
+    section = re.sub(r'([a-z])(\d)', r'\1-\2', section)
+    section = re.sub(r'(\d)([a-z])', r'\1-\2', section)
+    section = re.sub(r'section', '', section).strip()
+
+    abbreviations = {
+        'flr': 'floor',
+        'orch': 'orchestra',
+        'blch': 'bleacher',
+        'fd': 'field',
+        'balc': 'balcony',
+        'rgt': 'right',
+        'lft': 'left',
+        'frn': 'front',
+        'ctr': 'center'
+    }
+    for abbr, full in abbreviations.items():
+        section = section.replace(abbr, full)
+    
+    if original_section != section:
+        logging.info(f"Normalized '{original_section}' to '{section}'")
+    
+    return section
+
+def apply_rules(section, rules, all_sections):
+    """
+    Apply transformation rules to the section name.
+    """
+    original_section = section
+
+    # Apply hardcoded normalization
+    section = normalize_section_name(section)
+
+    # Check for a unique three-digit number
+    three_digit_num = extract_three_digit_number(section)
+    if three_digit_num:
+        matching_sections = [s for s in all_sections if three_digit_num in s]
+        if len(matching_sections) == 1:
+            return matching_sections[0]
+
+    # Apply custom rules from the rules.txt
+    for pattern, replacement in rules.items():
+        if re.search(pattern, section):
+            section = re.sub(pattern, replacement, section)
+            logging.info(f"Applied rule on '{original_section}': {pattern} > {replacement}. Result: '{section}'")
+
+    if section != original_section and section in all_sections:
+        logging.info(f"Final transformation for '{original_section}' is '{section}'")
+
+    return section
+
+def load_csv(filename, rules, all_sections=None):
+    data = {}
+    
+    with open(filename, 'r') as f:
+        # Splitting manually on newline and comma
+        rows = [line.strip().split(',') for line in f.readlines()]
+
+        # Extracting header from the first row and normalizing to lowercase
+        headers = [header.lower() for header in rows[0]]
+        
+        # Check if headers are as expected
+        expected_headers = ['section', 'row']
+        if 'seat' in headers:
+            expected_headers.append('seat')
+        
+        if headers != expected_headers:
+            raise ValueError(f"Unexpected headers in {filename}: {headers}")
+
+        # Iterate over the rows (excluding header)
+        for row in rows[1:]:
+            row_data = dict(zip(headers, row))
+            section = apply_rules(row_data['section'], rules, all_sections)
+            
+            if 'seat' in headers:
+                if section not in data:
+                    data[section] = {}
+                
+                # Convert the row name to lowercase
+                row_name_lower = row_data['row'].lower()
+                
+                if row_name_lower not in data[section]:
+                    data[section][row_name_lower] = []
+                data[section][row_name_lower].append(row_data['seat'])
+            else:
+                if section not in data:
+                    data[section] = set()
+                data[section].add(row_data['row'].lower())
+
+    return data
+
+
+
+def compare_manifests(mine, client):
+    differences = []
+
+    # Check if comparison is seat-level or row-level
+    is_seat_level = all(isinstance(val, dict) for val in mine.values())
+
+    if is_seat_level:
+        # Seat-level comparison logic
+        matched_sections = set()  # To track sections that matched
+        
+        for section in mine:
+            if section not in client:
+                differences.append(f"section {section} missing in client's manifest")
+                continue
+            matched_sections.add(section)
+            for row in mine[section]:
+                row_lower = row.lower()
+                if row_lower not in client[section]:
+                    differences.append(f"section {section}, row {row} missing in client's manifest")
+                    continue
+
+                missing_in_mine = set(client[section][row_lower]) - set(mine[section][row])
+                missing_in_client = set(mine[section][row]) - set(client[section][row_lower])
+
+                if missing_in_mine:
+                    differences.append(f"In section {section}, row {row}, client's manifest has seats {', '.join(map(str, missing_in_mine))} that are not in SeatGeek Manifest.")
+                if missing_in_client:
+                    differences.append(f"In section {section}, row {row}, SeatGeek Manifest has seats {', '.join(map(str, missing_in_client))} that are not in client's manifest.")
+
+        unmatched_sections = set(client.keys()) - matched_sections
+        for section in unmatched_sections:
+            differences.append(f"section {section} missing in SeatGeek Manifest")
+
+    else:
+        # Row-level comparison logic
+        missing_sections_in_mine = set(client.keys()) - set(mine.keys())
+        missing_sections_in_client = set(mine.keys()) - set(client.keys())
+
+        for section in missing_sections_in_mine:
+            differences.append(f"section {section} missing in SeatGeek Manifest")
+
+        for section in missing_sections_in_client:
+            differences.append(f"section {section} missing in client's manifest")
+
+        for section, rows in mine.items():
+            if section in client:
+                missing_rows_in_mine = client[section] - rows
+                missing_rows_in_client = rows - client[section]
+
+                for row in missing_rows_in_mine:
+                    differences.append(f"In section {section}, row {row} missing in SeatGeek Manifest")
+
+                for row in missing_rows_in_client:
+                    differences.append(f"In section {section}, row {row} missing in client's manifest")
+
+    return differences
+
+"""if __name__ == "__main__":
+    logging.info("Script started.")
+
+    rules = load_rules('rules.txt')
+    seatgeek_manifest_sections = set()
+    
+    with open('seatgeek_manifest.csv', 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            # Making it case-insensitive
+            section_value = row.get('SECTION', row.get('section', '')).lower()
+            seatgeek_manifest_sections.add(section_value)
+            
+    seatgeek_manifest = load_csv('seatgeek_manifest.csv', rules, seatgeek_manifest_sections)
+    client_manifest = load_csv('client_manifest.csv', rules, seatgeek_manifest_sections)
+    
+    differences = compare_manifests(seatgeek_manifest, client_manifest)
+
+    with open('differences.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(["Difference"])
+        for diff in differences:
+            writer.writerow([diff])
+
+    logging.info("Script finished. Results written to 'differences.csv'")"""
+
+if __name__ == "__main__":
+    logging.info("Script started.")
+
+    rules = load_rules('rules.txt')
+    seatgeek_manifest_sections = set()
+    
+    with open('temp_seatgeek.csv', 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            # Making it case-insensitive
+            section_value = row.get('SECTION', row.get('section', '')).lower()
+            seatgeek_manifest_sections.add(section_value)
+            
+    seatgeek_manifest = load_csv('temp_seatgeek.csv', rules, seatgeek_manifest_sections)
+    client_manifest = load_csv('temp_client.csv', rules, seatgeek_manifest_sections)
+    
+    differences = compare_manifests(seatgeek_manifest, client_manifest)
+
+    with open('differences.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(["Difference"])
+        for diff in differences:
+            writer.writerow([diff])
+
+    logging.info("Script finished. Results written to 'differences.csv'")
